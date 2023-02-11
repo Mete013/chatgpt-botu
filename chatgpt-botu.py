89bc547e11894f05dafef40a1119f7c71f92aff4
Qasım Məmmədov <71598037+Mete013@users.noreply.github.com>
2023-02-11 16:03:10 +0400

    Update chatgpt-botu.py

--- chatgpt-botu.py
+++ chatgpt-botu.py
@@ -1,38 +1,11 @@
-import logging
-import telegram
 from telegram.ext import Updater, CommandHandler
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-# Define a function to respond to the /start command
 def start(update, context):
-    update.message.reply_text("Salam!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Salam!")
 
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+updater = Updater(token="1801568916:AAEToeCtmwjKkuU2mv9Eb9ff0xF9w4vUUvI", use_context=True)
 
-# Create the Updater and pass it your bot's token
-TOKEN = "1801568916:AAEToeCtmwjKkuU2mv9Eb9ff0xF9w4vUUvI"
+start_handler = CommandHandler("start", start)
+updater.dispatcher.add_handler(start_handler)
 
-updater = Updater(TOKEN, use_context=True)
-
-# Get the dispatcher to register handlers
-dp = updater.dispatcher
-
-# Add the handler for the /start command
-dp.add_handler(CommandHandler("start", start))
-
-# log all errors
-dp.add_error_handler(error)
-
-# Start the Bot
 updater.start_polling()
-
-# Run the bot until you press Ctrl-C or the process receives SIGINT,
-# SIGTERM or SIGABRT. This should be used most of the time, since
-# start_polling() is non-blocking and will stop the bot gracefully.
-updater.idle()
