79ee1d4ff0dca83931f909a6836082be0e30630f
Qasım Məmmədov <71598037+Mete013@users.noreply.github.com>
2023-02-11 16:20:08 +0400

    Update chatgpt-botu.py

--- chatgpt-botu.py
+++ chatgpt-botu.py
@@ -1,11 +1,16 @@
+import telegram
 from telegram.ext import Updater, CommandHandler
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Salam!")
 
-updater = Updater(token="1801568916:AAEToeCtmwjKkuU2mv9Eb9ff0xF9w4vUUvI", use_context=True)
+def main():
+    updater = Updater(token="1801568916:AAEToeCtmwjKkuU2mv9Eb9ff0xF9w4vUUvI", use_context=True)
+    dp = updater.dispatcher
+    start_handler = CommandHandler("start", start)
+    dp.add_handler(start_handler)
+    updater.start_polling()
+    updater.idle()
 
-start_handler = CommandHandler("start", start)
-updater.dispatcher.add_handler(start_handler)
-
-updater.start_polling()
+if __name__ == '__main__':
+    main()
